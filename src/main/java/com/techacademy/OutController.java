1619031d7d7dc1931d7d64fabe1ba096fc952feb
Shibazaki Takahiro <Shibazaki Takahiro@LAPTOP-5BU4T3JV>
2023-03-21 21:51:23 +0900

    課題

---
+++ src/main/java/com/techacademy/OutController.java
@@ -0,0 +1,19 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class OutController {
+
+    @PostMapping("/output")
+    public String postOutput(@RequestParam("val") String val, Model model) {
+        model.addAttribute("val", val);
+
+        return "output";
+    }
+
+  }
+
